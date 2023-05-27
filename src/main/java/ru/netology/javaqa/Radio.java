5a79280e2d0c442915966d7db755fe52b7f93d45
Tervi test <Tervi@list.ru>
2023-05-27 18:03:16 +0300

    last

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -35,11 +35,32 @@ public class Radio {
         this.currentStation = currentStation;
     }
 
+
+
     private int currentVolume;
 
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        } else {
+            currentVolume = 100;
+        }
+    }
+
+    public void turnDownVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else {
+            currentVolume = 0;
+        }
+    }
+
+
     public int getCurrentVolume() {
+
         return currentVolume;
     }
+
 
     public void setCurrentVolume(int currentVolume) {
         if (currentVolume < 0) {
